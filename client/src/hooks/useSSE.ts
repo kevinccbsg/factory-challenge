9e87d4ee0e9eae1f423c7f77251910f8d40adcbe
kevinccbsg <kevinccbsg@gmail.com>
2024-01-27 01:49:45 +0100

    refactor: linter issues

--- client/src/hooks/useSSE.ts
+++ client/src/hooks/useSSE.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const useSSE = <T>(url: string) => {
   const [parts, setParts] = useState<T[]>([]);
---
+++ client/src/pages/Error/Error.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>505</h1>
+      <h2>Internal server error</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
