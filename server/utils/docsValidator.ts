560183f42c8e99ed1cf79d752966766600f8681a
kevinccbsg <kevinccbsg@gmail.com>
2024-01-27 01:45:30 +0100

    refactor: remove unnecesary server code

--- server/utils/docsValidator.ts
+++ server/utils/docsValidator.ts
@@ -1,19 +1,17 @@
 import { Express } from 'express';
 import expressJSDocSwagger, { Options } from 'express-jsdoc-swagger';
-import { init, ValidatorInstance } from 'express-oas-validator';
 
 interface Dependencies {
   app: Express;
   config: Options;
 }
 
-const initDocs = ({ config, app }: Dependencies): Promise<ValidatorInstance> =>
+const initDocs = ({ config, app }: Dependencies): Promise<void> =>
   new Promise((resolve, reject) => {
     const instance = expressJSDocSwagger(app)(config);
 
-    instance.on('finish', (swaggerDef) => {
-      const { validateRequest, validateResponse } = init(swaggerDef);
-      resolve({ validateRequest, validateResponse });
+    instance.on('finish', () => {
+      resolve();
     });
 
     instance.on('error', (error) => {
